25a2080cb6a1b9a89cb6e6f95c03e56db4256883
Nith <nitharson18@gmail.com>
2021-03-09 13:54:52 +0100

    Update App.java

--- IGB/src/App.java
+++ IGB/src/App.java
@@ -1,8 +1,41 @@
-public class App {
+/*public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, Team!");
         System.out.println("i am here");
         System.out.println("nith");
         System.out.println("nith2");    
     }
+} */
+
+public class Personne implements java.io.Serializable{
+    private String nom = "";
+    private String prenom ="";
+    private int taille = 0;
+
+    public Personne(final String nom, final String prenom, final int taille){
+        this.nom = nom;
+        this.taille = taille;
+        this.prenom = prenom;
+    }
+
+    public String getNom() {
+        return this.nom;
+    }
+    public void setNom(final String nom) {
+        this.nom = nom;
+    }
+    public int getTaille(){
+        return this.taille;
+    }
+
+    public void setTaille(final int taille) {
+        this.taille = taille;
+    }
+    public String getPrenom(){
+        return this.prenom;
+    }
+    public void setPrenom(final String prenom){
+        this.prenom = prenom;
+    }
 }
+
