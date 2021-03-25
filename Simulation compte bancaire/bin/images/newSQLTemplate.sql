2d761810f0b068a7afa97b3ce8370af6801c40db
Moun22 <78032868+Moun22@users.noreply.github.com>
2021-03-25 23:09:13 +0100

    Add files via upload

---
+++ Simulation compte bancaire/bin/images/newSQLTemplate.sql
@@ -0,0 +1,10 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ * Author:  mounirk2000
+ * Created: 25 mars 2021
+ */
+
